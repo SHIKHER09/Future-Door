ff9984689057037e18d5ffdeb728d96dbd61bcbb
Shikher Kumar <75657175+SHIKHER09@users.noreply.github.com>
2023-01-04 21:05:09 +0530

    Revert "FutureDoor"

--- Project/src/db/conn.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require ("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/newRegistration")
-.then(() => {
-    console.log(`connection successful....`);
-}).catch((err) => {
-        console.log(err);
-});
--- Project/src/key.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    MONGOURI:"mongodb+srv://yogita:XWenNvgOgZa2BPGE@cluster0.xr9s7yl.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET:"egeaijmiedomslspidnwmdkuftrvbzmzis"
-}
-
-//XWenNvgOgZa2BPGE
